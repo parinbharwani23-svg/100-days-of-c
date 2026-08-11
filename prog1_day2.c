eaf80c269ce14458ad96c5e0f9e0fdc2c6425726
parinbharwani23-svg <parinbharwani23@gmail.com>
2026-08-11 14:10:54 +0530

    Add files via upload

---
+++ prog1_day2.c
@@ -0,0 +1,12 @@
+ #include<stdio.h>
+ #include<math.h>
+ int main()
+{ 
+  int l,b,area,perimeter;
+  printf("Enter length and bredth:\n");
+  scanf("%d%d",&l,&b);
+  area=(l*b);
+  perimeter=2*(l+b);
+  printf("area=%d,perimeter=%d",area=l*b,perimeter=2*(l+b));
+  return 0;
+ }  
